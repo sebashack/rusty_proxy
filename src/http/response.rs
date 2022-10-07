a37256a03f8034d819cdefe5114ff3c0f50d9787
jdvalencit <jdvalencit@eafit.edu.co>
2022-10-07 17:12:15 -0500

    Refactor headers + response + request

--- src/http/response.rs
+++ src/http/response.rs
@@ -1,18 +1,10 @@
-use anyhow::{Error, Result};
+use anyhow::{Context, Error, Result};
 use std::collections::HashMap;
 use std::io::{prelude::*, BufReader};
 use std::net::TcpStream;
+use url::Url;
 
 use crate::http::headers;
-
-/*Response = Status-line
- *      *(( general-header
- *      | response-header
- *      | entity-header ) CRLF)
- *      CRLF
- *      [ message body ]
- *
- * Status-Line = HTTP-Version SP Status-Code SP Reason-Phrase CRLF   */
 
 #[derive(Debug, Clone)]
 pub enum Code {
@@ -123,7 +115,7 @@ pub struct Response {
 
 impl Response {
     pub fn from_tcp_stream(stream: &mut TcpStream) -> Result<Self> {
-        let (header, body) = Self::split_res(stream)?;
+        let (header, body) = split_res(stream)?;
         Ok(Response { header, body })
     }
 
@@ -133,57 +125,6 @@ impl Response {
         buffer.append(&mut body);
         buffer
     }
-
-    fn split_res(mut stream: &TcpStream) -> Result<(ResponseHeader, Vec<u8>)> {
-        let buff = BufReader::new(&mut stream);
-        let mut header_buff: Vec<u8> = Vec::new();
-        let mut body: Vec<u8> = Vec::new();
-        let mut crlfs = 0;
-        let mut it = buff.bytes();
-
-        while crlfs != 2 {
-            match it.next() {
-                Some(Ok(byte)) => {
-                    header_buff.push(byte);
-                    match byte {
-                        0x0D => {} //do nothing
-                        0x0A => {
-                            crlfs += 1;
-                        }
-                        _ => {
-                            crlfs = 0;
-                        }
-                    }
-                }
-                Some(Err(_)) => {
-                    return Err(Error::msg(format!("Error while reading response")));
-                }
-                None => {}
-            }
-        }
-
-        let header_str = String::from_utf8(header_buff)?;
-        let header = headers::parse_response_header(header_str.as_str())?;
-
-        if let Some(len) = header.get_content_length() {
-            let mut read_bytes = 0;
-
-            while read_bytes < len {
-                match it.next() {
-                    Some(Ok(byte)) => {
-                        body.push(byte);
-                        read_bytes += 1;
-                    }
-                    Some(Err(_)) => {
-                        return Err(Error::msg(format!("Error while reading request")));
-                    }
-                    None => {}
-                }
-            }
-        }
-
-        return Ok((header, body));
-    }
 }
 
 #[derive(Debug, Clone)]
@@ -191,6 +132,7 @@ pub struct ResponseHeader {
     pub status: StatusLine,
     pub headers: Headers,
 }
+
 impl ResponseHeader {
     pub fn get_content_length(&self) -> Option<usize> {
         if let Some(h) = self.headers.get("content-length") {
@@ -239,5 +181,155 @@ impl StatusLine {
         let line = [version, &sp, code, &sp, reason, &crlf].concat();
 
         line.to_vec()
+    }
+}
+
+fn split_res(mut stream: &TcpStream) -> Result<(ResponseHeader, Vec<u8>)> {
+    let buff = BufReader::new(&mut stream);
+    let mut header_buff: Vec<u8> = Vec::new();
+    let mut body: Vec<u8> = Vec::new();
+    let mut crlfs = 0;
+    let mut it = buff.bytes();
+
+    while crlfs != 2 {
+        match it.next() {
+            Some(Ok(byte)) => {
+                header_buff.push(byte);
+                match byte {
+                    0x0D => {} //do nothing
+                    0x0A => {
+                        crlfs += 1;
+                    }
+                    _ => {
+                        crlfs = 0;
+                    }
+                }
+            }
+            Some(Err(_)) => {
+                return Err(Error::msg(format!("Error while reading response")));
+            }
+            None => {}
+        }
+    }
+
+    let header_str = String::from_utf8(header_buff)?;
+    let header = parse_response_header(header_str.as_str())?;
+
+    if let Some(len) = header.get_content_length() {
+        let mut read_bytes = 0;
+
+        while read_bytes < len {
+            match it.next() {
+                Some(Ok(byte)) => {
+                    body.push(byte);
+                    read_bytes += 1;
+                }
+                Some(Err(_)) => {
+                    return Err(Error::msg(format!("Error while reading request")));
+                }
+                None => {}
+            }
+        }
+    }
+
+    return Ok((header, body));
+}
+
+pub fn parse_response_header(input: &str) -> Result<ResponseHeader> {
+    let (s, rest) = input
+        .split_once("\r\n")
+        .context(format!("Invalid request headers: {}", input))?;
+    let status = parse_status_line(format!("{s}\r\n").as_str())?;
+    let headers = headers::parse_headers(rest)?;
+
+    Ok(ResponseHeader { status, headers })
+}
+
+fn parse_status_line(input: &str) -> Result<StatusLine> {
+    let (s, rest) = input
+        .split_once(' ')
+        .context(format!("Invalid request-line: {}", input))?;
+    let version = headers::parse_version(s)?;
+    let (s, rest) = rest
+        .split_once(' ')
+        .context(format!("Invalid request-line: {}", input))?;
+    let code = parse_code(s)?;
+    let (s, _) = rest
+        .split_once("\r\n")
+        .context(format!("Invalid request-line: {}", input))?;
+    let reason = parse_reason(s)?;
+
+    Ok(StatusLine {
+        version: version.to_string(),
+        code: code,
+        reason: reason.to_string(),
+    })
+}
+
+fn parse_code(input: &str) -> Result<Code> {
+    match input {
+        "100" => Ok(Code::Code100),
+        "101" => Ok(Code::Code101),
+        "200" => Ok(Code::Code200),
+        "201" => Ok(Code::Code201),
+        "202" => Ok(Code::Code202),
+        "203" => Ok(Code::Code203),
+        "204" => Ok(Code::Code204),
+        "205" => Ok(Code::Code205),
+        "206" => Ok(Code::Code206),
+        "300" => Ok(Code::Code300),
+        "301" => Ok(Code::Code301),
+        "302" => Ok(Code::Code302),
+        "303" => Ok(Code::Code303),
+        "304" => Ok(Code::Code304),
+        "305" => Ok(Code::Code305),
+        "307" => Ok(Code::Code307),
+        "401" => Ok(Code::Code401),
+        "402" => Ok(Code::Code402),
+        "403" => Ok(Code::Code403),
+        "404" => Ok(Code::Code404),
+        "405" => Ok(Code::Code405),
+        "406" => Ok(Code::Code406),
+
+        "407" => Ok(Code::Code407),
+        "408" => Ok(Code::Code408),
+        "409" => Ok(Code::Code409),
+        "410" => Ok(Code::Code410),
+        "411" => Ok(Code::Code411),
+        "412" => Ok(Code::Code412),
+        "413" => Ok(Code::Code413),
+        "414" => Ok(Code::Code414),
+        "415" => Ok(Code::Code415),
+        "416" => Ok(Code::Code416),
+        "417" => Ok(Code::Code417),
+        "500" => Ok(Code::Code417),
+        "501" => Ok(Code::Code417),
+        "502" => Ok(Code::Code417),
+        "503" => Ok(Code::Code417),
+        "504" => Ok(Code::Code417),
+        "505" => Ok(Code::Code417),
+        fail_code => match fail_code.chars().nth(0).unwrap() {
+            '1' => Ok(Code::Code100),
+            '2' => Ok(Code::Code200),
+            '3' => Ok(Code::Code300),
+            '4' => Ok(Code::Code400),
+            '5' => Ok(Code::Code500),
+            _ => Err(Error::msg(format!("Invalid method: {:?}", input))),
+        },
+    }
+}
+
+fn parse_reason<'a>(input: &'a str) -> Result<&str> {
+    //parse text?
+    let prefix = if input.starts_with("/") {
+        "http://host"
+    } else {
+        ""
+    };
+
+    if let Ok(_) = Url::parse(format!("{}{}", prefix, input).as_str()) {
+        Ok(input)
+    } else {
+        Err(Error::msg(format!("Invalid request-uri: {:?}", input)))
     }
 }
