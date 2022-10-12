cbbcd06e72411e9b5ba47372694ddbf943f6d529
jdvalencit <jdvalencit@eafit.edu.co>
2022-10-12 16:57:39 -0500

    Cache cleaner implementation

--- src/cache/cleaner.rs
+++ src/cache/cleaner.rs
@@ -1,8 +1,10 @@
 use log::{error, info};
-use std::path::PathBuf;
+use std::path::{self, PathBuf};
 use std::sync::mpsc::Receiver;
 use std::thread::{self, JoinHandle};
-use std::time;
+use std::{fs, time};
+
+use super::io::{delete_cache_file, CacheFile};
 
 #[allow(dead_code)]
 pub struct CacheCleaner {
@@ -17,11 +19,31 @@ impl CacheCleaner {
         let thread = thread::spawn(move || loop {
             info!("Cleaning cache ...");
 
-            // TODO: Traverse cache_dir and delete expired files.
+            Self::traverse_files(cache_dir.clone());
 
             thread::sleep(one_min);
         });
 
         CacheCleaner { thread }
+    }
+
+    fn traverse_files(path: PathBuf) {
+        for entry in fs::read_dir(path.as_path()) {
+            for dir_entry in entry {
+                if let Ok(dir_entry) = dir_entry {
+                    let dir_entry_path = dir_entry.path();
+                    if dir_entry_path.is_file() {
+                        if let Ok(metadata) = CacheFile::read_header(&dir_entry_path) {
+                            if metadata.is_expired() {
+                                delete_cache_file(dir_entry_path)
+                                    .expect("Failed to delete expired cache file");
+                            }
+                        }
+                    } else {
+                        Self::traverse_files(dir_entry_path);
+                    }
+                }
+            }
+        }
     }
 }
